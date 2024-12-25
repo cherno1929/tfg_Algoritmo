31d17b3fcd49f24b65c4f83038205b7be5fa94e8
Cherno Valdor <y.labchiri.2021@alumnos.urjc.es>
2024-12-25 17:56:34 +0100

    --------------------------------- ---------------------------------
    Errors Solved ::
    Prim error were solved, now it doesnt give false positives/negatives
    
    ---------------------------------
    ---------------------------------

--- Algorithms/Checkers/Prim.py
+++ Algorithms/Checkers/Prim.py
@@ -1,33 +1,45 @@
+import copy
+
 from Algorithms.Checkers.Checker import Checker
 
 class Prim(Checker):
 
     def __select_next_node__(self, distances, visited):
-        bestItem = None
-        minDist = float("inf")
+        best_item = None
+        min_dist = float("inf")
         # Itarete until find the best node
         for i in range(len(distances)):
-            if i not in visited and distances[i] < minDist:
-                bestItem = i
-                minDist = distances[i]
-        return bestItem
+            if i not in visited and distances[i] < min_dist:
+                best_item = i
+                min_dist = distances[i]
+        return best_item
 
     def __get_conection_to_new_node__(self,g,n,v,c):
         connected = False
-        minDist = float("inf")
+        min_dist = float("inf")
         # Iterate to find lest distant connection
         for x, y in g.edges(n):
             cost = g.get_edge_data(x, y)['dist']
             error = g.get_edge_data(x, y)['isFail']
             # It's known that the connection is feassible
-            if not error and y in v and cost < minDist and cost + c[y] <= g.graph['l_max']:
+            if not error and y in v and cost < min_dist and cost + c[y] <= g.graph['l_max']:
                 # If is a generator then the distance resets to 0
+                min_dist = cost + c[y]
                 if g.nodes[n]['isGen']:
                     c[n] = 0
                 else:
-                    c[x] = cost + c[y]
+                    c[n] = min_dist
                 connected = True
         return connected  # So we know if the node conected
+
+    def __get_viable_dist_to_network__(self, g, end, visited, costs):
+        dist = float("inf")
+        while dist > g.graph['l_max'] and visited:
+            node = visited.pop()
+            if g.has_edge(node, end):
+                # Min dist and distance to network + cost of the visited node
+                dist = min(dist, g.get_edge_data(node, end)['dist'] + costs[node])
+        return dist
 
     def prim(self, g, node, len_g = float("inf")):
         n = len(g.nodes)
@@ -41,24 +53,36 @@ class Prim(Checker):
 
         visited = {node}
 
+        ###############################
+
         while n > 0 and len(visited) < len_g:
             # Select the next best node
             next_node = self.__select_next_node__(distances, visited)
             if next_node != None:
                 # Establish connection between new_node -- graph
                 if self.__get_conection_to_new_node__(g, next_node, visited, costs):
+                    # Add visited node
+                    visited.add(next_node)
                     # Update distances
                     for start, end in g.edges(next_node):
+                        if end not in visited:
+                            dist = self.__get_viable_dist_to_network__(g, end, list(visited),costs)  # g.get_edge_data(start, end)['dist']
+                            if dist <= g.graph['l_max']:
+                                distances[end] = min(dist, distances[end])
+                        '''
                         dist = g.get_edge_data(start, end)['dist']
                         if end not in visited and costs[next_node] + dist <= g.graph['l_max']:
                             distances[end] = min(dist, distances[end])
-                    visited.add(next_node)
+                        '''
                 else:
                     distances[next_node] = float("inf")
                     n += 1
             else:
                 break
             n -= 1
+
+        ###############################
+
         distances[node] = 0
         return visited, distances
 
